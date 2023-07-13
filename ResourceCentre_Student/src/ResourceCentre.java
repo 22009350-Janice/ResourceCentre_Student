cbbdc7d4f6fa3341361f89427da31305ac66adbd
ben50 <ben50@10.175.4.155>
2023-07-13 11:08:39 +0800

    changed comment

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class ResourceCentre {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//jgkgkjk
 
 		ArrayList<Camcorder> camcorderList = new ArrayList<Camcorder>();
 		ArrayList<Chromebook> chromebookList = new ArrayList<Chromebook>();
